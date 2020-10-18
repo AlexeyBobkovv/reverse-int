fb5ef43fe5de6d0f4801dafe7f4888f717a23b13
Just_Alexey <60109266+AlexeyBobkovv@users.noreply.github.com>
2020-10-18 13:43:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
   let result = 0;
-   n *=  -1
-  while (n !== 0) {
+ 	n = Math.abs(n)
+  while (n  > 0) {
     result = result * 10 + n % 10;
     n = Math.floor(n / 10);
   }
